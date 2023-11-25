08859bf852e63ad80ddf388e842e701af18b8b1c
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-11-25 16:42:30 +0500

    Task4

---
+++ Tyuiu.VorobevSA.Sprint5.Task4.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VorobevSA.Sprint5.Task4.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VorobevSA.Sprint5.Task4.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2546c96c-1c31-4c6a-8d20-70ffe59066a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VorobevSA.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.VorobevSA.Sprint5.Task4.V21.Lib;
+
+namespace Tyuiu.VorobevSA.Sprint5.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService service1 = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Воробьев С.А. | ПКДб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:Оператор цикла continue/break                                      *");
+            Console.WriteLine("* Задание #5.4                                                            *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил:Воробьев Сергей Александрович | ПКТб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле . Вычислить *");
+            Console.WriteLine("* значение по формуле (Полученное значение округлить до трёх знаков после *");
+            Console.WriteLine("* запятой) и вернуть полученный результат на консоль.                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V21.txt";
+            Console.WriteLine("Данные в файле:" + path);
+            Console.WriteLine("x= " + File.ReadAllText(path).Replace('.',','));
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(service1.LoadFromDataFile(path));
+
+            Console.ReadKey();
+        }
+    }
+}
